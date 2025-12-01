dfa476d6aea095848fdb994131371c3efcc62ddf
Josh Rice <josh.rice193@gmail.com>
2025-12-01 01:22:47 -0500

    Implementing Mic class.

---
+++ src/microphone.cpp
@@ -0,0 +1,120 @@
+#include "microphone.h"
+
+Microphone::Microphone(){
+    next_index = 0;
+    recording_buffer = nullptr;
+}
+
+Microphone::~Microphone(){
+    if(recording_buffer != nullptr){
+        free(recording_buffer);
+    }
+}
+  
+
+bool Microphone::Init(){
+    
+    //Yell at them for calling Init() twice.
+    if(recording_buffer != nullptr){
+        return false;
+    }
+
+    //Allocate (BUFFER_SIZE * 2) bytes of memory for our circular buffer.
+    // *2 cuz its 16-bit int, not 8.
+    recording_buffer = (typeof(recording_buffer))heap_caps_malloc(
+        RECORDING_BUFFER_SIZE * sizeof(int16_t), MALLOC_CAP_8BIT);
+
+    //If allocation failed, return false.
+    if(recording_buffer == NULL){
+        return false;
+    }
+
+    //Init all memory to 0
+    memset(recording_buffer, 0, RECORDING_BUFFER_SIZE * sizeof(int16_t));
+
+    return true;
+}
+
+
+//Record a single block of audio samples
+    //This is basically one block of size "SAMPLES_IN_RECORDING_BLOCK"
+
+//I *think* the usage is:
+    //1. Record every loop.
+    //2. Increment NEXT_INDEX back in main if this function returns true
+    //3. Only send when our NEXT_INDEX rolls over.
+bool Microphone::RecordOneBlock(){
+
+    //Return early
+    if(!M5.Mic.isEnabled()){
+        return false;
+    }
+
+    //Set a pointer to recording_buffer[NEXT_INDEX];
+    auto data = &recording_buffer[next_index * RECORDING_SAMPLES_PER_BLOCK];
+
+    //Write accumulated recording data.
+    //If false, I think that means something went wrong down in the boiler room? 
+    if(!M5.Mic.record(data, RECORDING_SAMPLES_PER_BLOCK, RECORDING_SAMPLE_RATE)){
+        return false;
+    }
+
+    //Increment circular buffer
+    next_index++;
+    if (next_index >= RECORDING_BLOCK_COUNT){
+        next_index = 0;
+    }
+
+    return true;    
+}
+
+
+
+//Copy to Mqtt buffer.
+//It felt wrong to do the actual publishing in here.
+bool Microphone::WriteToMqttBuffer(char* buffer, size_t buffer_size){
+    
+    const size_t total_samples = RECORDING_BUFFER_SIZE;
+    const size_t bytes_needed = total_samples * sizeof(int16_t);
+
+    if(recording_buffer == nullptr){
+        Serial.println("ERR:Microphone:WriteToMqttBuffer: recording_buffer is null. Did you forget to initialize?");
+        return false;
+    }
+
+    //Is this an error state? Or should we just kind of send what we can..
+    if(buffer_size < bytes_needed){
+        Serial.print("ERR:Microphone:WriteToMqttBuffer: Tried to write ");
+        Serial.print(bytes_needed);
+        Serial.print(" bytes to a buffer of size ");
+        Serial.println(buffer_size);
+        return false;
+    }
+    
+    //Find where our "Next Index" is.
+    size_t starting_block = next_index;
+    size_t starting_sample = starting_block * RECORDING_SAMPLES_PER_BLOCK;
+
+    //Grab current index --> end of circular buffer
+    size_t first_half_sample_count = total_samples - starting_sample;
+    //Grab the count of bytes in the first half.
+    size_t first_half_byte_count = first_half_sample_count * sizeof(int16_t);
+
+    //////// COPY FROM NEXT INDEX --> IDX[MAX] //////
+    memcpy(buffer, 
+        &recording_buffer[starting_sample], 
+        first_half_byte_count);
+
+    ////// COPY FROM IDX[0] --> NEXT INDEX /////
+    if(starting_sample > 0){
+        size_t second_half_sample_count = starting_sample;
+        size_t second_half_byte_count = second_half_sample_count * sizeof(int16_t);
+
+        memcpy(buffer + first_half_byte_count,
+        &recording_buffer[0],
+        second_half_byte_count);
+    }
+
+    return true;
+}
+
--- src/microphone.h
+++ src/microphone.h
@@ -1,6 +1,38 @@
 #ifndef MICROPHONE_H
 #define MICROPHONE_H
+#include "gui.h"
+
+//REMEMBER:
+    //IF SOMETHING IS NOT WORKING - DOUBLE CHECK SPEAKER HAS RELINQUISHED CONTROL
+
+static constexpr const size_t RECORDING_BLOCK_COUNT             = 200;
+static constexpr const size_t RECORDING_SAMPLES_PER_BLOCK       = 240;
+
+static constexpr const size_t RECORDING_BUFFER_SIZE         = RECORDING_BLOCK_COUNT * RECORDING_SAMPLES_PER_BLOCK;
+static constexpr const size_t RECORDING_SAMPLE_RATE         = 22050;
 
 
+class Microphone{
+    public:
+        Microphone();
+        ~Microphone();
+
+        bool Init();
+        bool RecordOneBlock();
+        bool WriteToMqttBuffer(char* buffer, size_t buffer_size);
+
+        int16_t GetRecordingBuffer();
+        size_t GetNextIndex();
+
+    private:
+        int16_t *recording_buffer;//Pointer to the samples buffer [record_number * record_length]
+        size_t next_index;//Next index to record to
+};
+
+
+//Returns allocated buffer initialized to 0
+boolean initRecordingCircularBuffer(int16_t* recording_buffer, size_t buffer_size);
+
+boolean recordOneBlock(int16_t* recording_buffer, size_t next_index, size_t buffer_size);
 
 #endif
